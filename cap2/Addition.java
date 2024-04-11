19019ca1d7155aa3fabee814fa3190fc2ee26d01
gsbad <gsbadbr@gmail.com>
2024-04-10 22:17:00 -0300

    exercicios de revisao do capitulo 2

---
+++ cap2/Addition.java
@@ -0,0 +1,22 @@
+public class Addition{
+
+    public static void main(String args[]){
+
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.print("Digite um número: ");
+        number1 = input.nextInt();
+
+        System.out.print("Digite um número: ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("A soma dos dois números é: %d%n", sum);
+    }
+
+}
