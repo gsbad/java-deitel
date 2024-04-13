ee93c11e752c1d8383ab9af42208a734b9a8976a
gsbad <gsbadbr@gmail.com>
2024-04-12 23:50:17 -0300

    Inicialização de objetos Account com o construtor

--- cap2/Account.java
+++ cap2/Account.java
@@ -2,13 +2,37 @@
 public class Account{
 
     private String name;
+    private double balance;
+
+    public Account(String name, double balance){
+        
+        this.name = name;
+        
+        if(balance >= 0){
+            this.balance = balance;
+        }
+    }
+
+    // método que deposita (adiciona) apenas uma quantia válida no saldo
+    public void deposit(double depositAmount)
+    {
+        if (depositAmount > 0.0) // se depositAmount for válido
+        balance = balance + depositAmount; // o adiciona ao saldo
+    }
+    // método retorna o saldo da conta
+    public double getBalance()
+    {
+        return balance;
+    }
 
     public void setName(String name){
-        this.name = name;
+        this.name = name;   
     }
 
     public String  getName(){
         return name;
     }
+
+
 
 }
--- cap2/AccountTest.java
+++ cap2/AccountTest.java
@@ -5,17 +5,39 @@ public class AccountTest
     public static void main(String args[]){
 
         Scanner input = new Scanner(System.in); 
-        Account myAccount = new Account();
 
-        System.out.printf("%s%n", myAccount.getName());
+        Account account1 = new Account("Gustavo Sá", 15);
+        Account account2 = new Account("Miguel Sá", 15.7);
 
-        System.out.print("Por favor insira seu nome: ");
-        String theName = input.nextLine();
+        // exibe saldo inicial de cada objeto
+        System.out.printf("%s balance: $%.2f %n",
+        account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+        account2.getName(),account2.getBalance());
 
-        myAccount.setName(theName);
+        System.out.print("Enter deposit amount for account1: "); // prompt
+        double depositAmount = input.nextDouble(); // obtém a entrada do usuário
+        System.out.printf("%nadding %.2f to account1 balance%n%n",
+        depositAmount);
+        account1.deposit(depositAmount); // adiciona o saldo de account1
 
-        System.out.println();
-        System.out.printf("Nome no objeto myAccount é: %n%s%n", myAccount.getName());
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n",
+        account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+        account2.getName(),account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: "); // prompt
+        depositAmount = input.nextDouble(); // obtém a entrada do usuário
+        System.out.printf("%nadding %.2f to account2 balance%n%n",
+        depositAmount);
+        account2.deposit(depositAmount); // adiciona ao saldo de account2
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n",
+        account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+        account2.getName(),account2.getBalance());
 
     }
 }
