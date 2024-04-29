19ebebfa3440f5a1e57d4f2bee6fd3170ade68d9
gsbad <gsbadbr@gmail.com>
2024-04-29 16:14:15 -0300

    SalariedEmployee.java

---
+++ cap10/SalariedEmployee.java
@@ -0,0 +1,41 @@
+package cap10;
+
+public class SalariedEmployee extends Employee{
+
+    //váriavel de instância espécifica da subclasse
+    private double weeklySalary;
+
+    //Construtor
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, double weeklySalary){
+        super(firstName, lastName, socialSecurityNumber);
+        
+        //validações
+        if(weeklySalary < 0.0) 
+            throw new IllegalArgumentException("Weekly salary must be >= 0!");
+        this.weeklySalary = weeklySalary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Salaried Employee: %s%n%s: $%,.2f",
+                    super.toString() , "weekly salary" , getWeeklySalary());
+    }
+
+    @Override
+    public double earnings() {
+        return getWeeklySalary();
+    }
+
+    public void setWeeklysalary(double weeklySalary){
+        //validações
+        if(weeklySalary < 0.0) 
+            throw new IllegalArgumentException("Weekly salary must be >= 0!");
+        this.weeklySalary = weeklySalary;
+    }
+
+    public double getWeeklySalary(){
+        return weeklySalary;
+    }
+
+    
+}
