09d08f5395ac98d28a8b2ceb8ef9f563d89a8787
gsbad <gsbadbr@gmail.com>
2024-05-03 12:29:28 -0300

    cap 5, 6, 7, 8, 9

---
+++ cap8/EmployeeOld.java
@@ -0,0 +1,22 @@
+public class EmployeeOld
+{
+   private String firstName;
+   private String lastName;
+   private Date birthDate;
+   private Date hireDate;
+
+   public EmployeeOld(String firstName, String lastName, Date birthDate, 
+      Date hireDate)
+   {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.birthDate = birthDate;
+      this.hireDate = hireDate;
+   } 
+
+   public String toString()
+   {
+      return String.format("%s, %s  Hired: %s  Birthday: %s", 
+         lastName, firstName, hireDate, birthDate);
+   } 
+}
