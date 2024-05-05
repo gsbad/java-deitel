1ccd96dd064fdcbd3bc9a28f579fa364e3cee5b2
gsbad <gsbadbr@gmail.com>
2024-05-05 10:37:28 -0300

    employee.test

--- cap8/EmployeeTest.java
+++ cap8/EmployeeTest.java
@@ -1,11 +1,9 @@
-import cap10.Employee;
-
 public class EmployeeTest 
 {
    public static void main(String[] args)
    {
       System.out.printf("Employees before instantiation: %d%n",
-         Employee.getCount());
+      Employee.getCount());
 
       Employee e1 = new Employee("Susan", "Baker");
       Employee e2 = new Employee("Bob", "Blue");
