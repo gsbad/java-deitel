0beecf2bd082416a67b670cf5e00cea23d5b1fc4
Gustavo Sá <gsbadbr@gmail.com>
2024-06-11 03:21:21 -0300

    Construtores StringBuilder e metodos da classe StringBuilder: length, setLength, capacity, ensureCapacity

--- cap14/StringBuilderConstructor.java
+++ cap14/StringBuilderConstructor.java
@@ -9,5 +9,19 @@ public class StringBuilderConstructor {
         System.out.printf("%nb1 = \"%s\"", b1);
         System.out.printf("%nb2 = \"%s\"", b2);
         System.out.printf("%nb3 = \"%s\"", b3);
+
+        //testando metodos da classe StringBuilder: length, setLength, capacity, ensureCapacity
+        StringBuilder b4 = new StringBuilder("Dune 2 is a masterpiece of modern cinema!");
+
+        System.out.printf("%nb4 = \"%s\"%nlength = %d%ncapacity = %d%n", b4.toString(), b4.length(), b4.capacity());
+
+        //nova capacidade
+        b4.ensureCapacity(77);
+        System.out.printf("%nnew capacity = %d%n", b4.capacity());
+
+        //novo tamanho
+        b4.setLength(23);
+        System.out.printf("%nnew length = %d%nb4 = \"%s\"", b4.length(), b4.toString());
+
     }
 }
