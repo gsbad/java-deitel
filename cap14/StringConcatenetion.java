4a607f2844798d3157b217693450e3475e6efebf
Gustavo Sá <gsbadbr@gmail.com>
2024-06-11 02:55:53 -0300

    +4 exemblos do cap14 sobre tratamento de Strings

---
+++ cap14/StringConcatenetion.java
@@ -0,0 +1,13 @@
+package cap14;
+
+public class StringConcatenetion {
+    public static void main(String[] args) {
+        //testa método concat()
+        String s1 = "Good ";
+        String s2 = "Morning!";
+
+        System.out.printf("%n%ns1.concat(s2) = %s%n", s1.concat(s2));
+        System.out.printf("%ns1 = %s%n", s1);
+        System.out.printf("%ns2 = %s%n", s2);
+    }
+}
---
+++ cap14/SubString.java
@@ -0,0 +1,10 @@
+package cap14;
+
+public class SubString{
+    public static void main(String[] args) {
+        String s1 = "abcdefghijklmnopqrsuvxyz";
+
+        System.out.printf("%n%ns1.substring(20) = %s", s1.substring(20));
+        System.out.printf("%n%ns1.substring(3, 6) = %s", s1.substring(3, 6));
+    }
+}
