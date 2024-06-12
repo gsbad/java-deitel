4a7df4b30c98dfe93bf8346a4ec4747e9997239f
gsbad <gsbadbr@gmail.com>
2024-06-12 20:19:18 -0300

    exemplos do apendice para rodar o jdb java debuuger

---
+++ apE/Hello.java
@@ -0,0 +1,12 @@
+package apE;
+
+public class Hello {
+    public static void main(String[] args) {
+        int n1 = 5;
+        int n2 = 2;
+        int n3 = 11;
+        int soma = n1 + n2 + n3;
+
+        System.out.println(soma);
+    }
+}
