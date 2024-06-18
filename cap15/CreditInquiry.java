0e3b8e237c0d01ac57f2ffa666a46b61cbcb7c72
Gustavo Sá <gsbadbr@gmail.com>
2024-06-18 11:46:41 -0300

    CredityInquiry.java cap15

---
+++ cap15/CreditInquiry.java
@@ -0,0 +1,89 @@
+package cap15;
+
+import java.io.IOException;
+import java.lang.IllegalStateException;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class CreditInquiry {
+
+    private final static MenuOption[] choices = MenuOption.values();
+
+
+
+    public static void main(String[] args) {
+        //obtem a solicitacao do usuario
+        MenuOption accountType = getRequest();
+
+        while (accountType != MenuOption.END) {
+            switch (accountType) {
+                case ZERO_BALANCE:
+                System.out.printf("%nAccounts with zero balances:%n");  
+                    break;
+                case CREDIT_BALANCE:
+                System.out.printf("%nAccounts with credit balances:%n");    
+                    break;
+                case DEBIT_BALANCE:
+                System.out.printf("%nAccounts with debit balances:%n");    
+                    break;            
+            }
+            readRecords(accountType);
+            accountType = getRequest(); //Recebe a entrada do usuario até q se escolha (4 END)    
+        }
+    }
+
+
+
+    private static MenuOption getRequest() {
+        int request = 4;
+
+        // exibe opções de solicitação
+        System.out.printf("%nEnter request%n%s%n%s%n%s%n%s%n",
+            " 1 - List accounts with zero balances",
+                " 2 - List accounts with credit balances",
+                " 3 - List accounts with debit balances",
+                " 4 - Terminate program");
+        try {
+            Scanner input = new Scanner(System.in);
+            do {
+                System.out.printf("%n? ");
+                request = input.nextInt();
+            } while (request < 1 || request > 4); //o laço acontece enquanto o usuario n colocar uma entrada de 1 a 4
+        } catch (NoSuchElementException e) {
+            System.err.println("Invalid input. Terminating.");
+        }
+        return choices[request - 1];
+    }
+
+
+    private static void readRecords(MenuOption accountType) {
+        try (Scanner input = new Scanner(Paths.get("cap15/clients.txt"))) {
+            while (input.hasNext()) {
+                int accountNumber = input.nextInt();
+                String firstName = input.next();
+                String lastName = input.next();
+                double balance = input.nextDouble();
+                
+                // se o tipo for a conta adequada, exibe o registro
+                if (shouldDisplay(accountType, balance)) 
+                    System.out.printf("%-10d%-12s%-12s%10.2f%n", accountNumber,
+                    firstName, lastName, balance);
+                else
+                    input.nextLine(); // descarta o restante do registro atual                
+            }
+        } catch (NoSuchElementException |  IllegalStateException | IOException e) {
+            System.err.println("Error processing file. Terminating.");
+            System.exit(1);
+        }
+    }
+    private static boolean shouldDisplay(MenuOption accountType, double balance){
+        if ((accountType == MenuOption.CREDIT_BALANCE) && (balance < 0))
+            return true;
+        else if ((accountType == MenuOption.DEBIT_BALANCE) && (balance > 0))
+            return true;
+        else if ((accountType == MenuOption.ZERO_BALANCE) && (balance == 0))
+            return true;     
+        return false;
+    }
+}
