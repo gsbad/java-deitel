65f281fd6cdfe079ffc1c4ef09f60566b5af7661
gsbad <gsbadbr@gmail.com>
2024-06-25 18:38:34 -0300

    correcao de um exemplo e .vscode/launch.json

--- classesobjetos/MemberAccessTest.java
+++ classesobjetos/MemberAccessTest.java
@@ -4,9 +4,7 @@ public class MemberAccessTest
    public static void main(String[] args)
    {
       Time time = new Time();
-
-      time.hour = 7;
-      time.minute = 15;
-      time.second = 30; 
+      time.setTime(7, 55, 40);
+     
    } 
 }
